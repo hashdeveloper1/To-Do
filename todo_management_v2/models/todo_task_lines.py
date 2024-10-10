4e858b1b675421c22d76ba2e57d58a079c810273
Hashem Ahmed Mahmoud <hashemodoodeveloper@gmail.com>
2024-10-10 17:01:59 +0300

    Update project on branch 2.0

--- todo_management_v2/models/todo_task_lines.py
+++ todo_management_v2/models/todo_task_lines.py
@@ -1,26 +1,23 @@
 from odoo import fields, models, api
 
 
-class TodoTask(models.Model):
-    _name = 'todo.task'
+class TodoTaskLines(models.Model):
+    _name = 'todo.task.lines'
     _inherit = ['mail.thread', 'mail.activity.mixin']
-    _description = 'To-Do Task'
+    _description = 'To-Do Task Lines'
     _order = 'id desc'
-    _rec_name = 'ref'
 
-    ref = fields.Char(compute="_compute_task_name", default='new', tracking=1)
     name = fields.Char(string="Task Name", tracking=1)
+    task_id = fields.Many2one('todo.task')
     assign_to_id = fields.Many2one('res.partner', required=True, states={'completed': [('readonly', True)], 'in_progress': [('readonly', True)]}, tracking=1)
-    due_date = fields.Datetime(required=True, states={'completed': [('readonly', True)], 'in_progress': [('readonly', True)]}, tracking=1)
+    time = fields.Float()
+    # due_date = fields.Date(required=True, states={'completed': [('readonly', True)], 'in_progress': [('readonly', True)]}, tracking=1)
     state = fields.Selection([
         ('new', 'New'),
         ('in_progress', 'In Progress'),
         ('completed', 'Completed')],
         default='new', tracking=1
     )
-    # description = fields.Char(states={'completed': [('readonly', True)]})
-    description = fields.Text(states={'completed': [('readonly', True)]}, tracking=1)
-    project_id = fields.Many2one('todo.project')
 
     def action_in_progress(self):
         for rec in self:
@@ -29,7 +26,3 @@ class TodoTask(models.Model):
     def action_completed(self):
         for rec in self:
             rec.state = 'completed'
-
-    def _compute_task_name(self):
-        for rec in self:
-            rec.ref = 'Task ' + str(rec.id)
